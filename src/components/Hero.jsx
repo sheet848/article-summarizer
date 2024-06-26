d57016b899de27189da86c326bff3e7d25f8c3b4
sheet848 <sheetalnaik848@gmail.com>
2024-06-27 01:02:37 +0530

    new updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <>
     <header className='w-full flex justify-center items-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-        <img src={logo} alt='sumz_logo' className='w-28 object-contain'/>
+        <img src={logo} alt='sumz_logo' className='w-28 object-contain invisible'/>
 
         <button type='button' className='black_btn' onClick={() => window.open('https://github.com/sheet848')}>
             GitHub
@@ -17,7 +17,7 @@ const Hero = () => {
         <span className='orange_gradient'>OpenAI GPT-4</span>
       </h1>
       <h2 className='desc'>
-        Simplify your reading with Summize, an open-source article summarizer that transforms lengthy articles into clear and concise summaries 
+        Simplify your reading with this article summarizer and transforms lengthy articles into clear and concise summaries 
       </h2>    
     </header>
     </>
