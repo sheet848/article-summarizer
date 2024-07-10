6ab33b0aea6dd82cb3b2809bb9a0032f703bea14
Sheetal Naik <sheetalnaik848@gmail.com>
2024-07-10 23:06:01 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,12 +4,12 @@ const Hero = () => {
   return (
     <>
     <header className='w-full flex justify-center items-center flex-col'>
-      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-        <img src={logo} alt='sumz_logo' className='w-28 object-contain invisible'/>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3 h-24'>
+        {/*<img src={logo} alt='sumz_logo' className='w-28 object-contain invisible'/>
 
         <button type='button' className='black_btn' onClick={() => window.open('https://github.com/sheet848')}>
             GitHub
-        </button>
+        </button> */}
       </nav>
 
       <h1 className='head_text'>
